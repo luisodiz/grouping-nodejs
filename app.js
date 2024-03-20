6095824f3f366f5b78c6d8701882a64f0572212b
Alexander Sosov <luisodiz@yandex.ru>
2024-03-20 21:59:40 +0300

    модули common js

--- app.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs')
-
-const data = fs.readFileSync('./data.txt')
-console.log(data.toString())
---
+++ lessons/modules/commonjs/characters.js
@@ -0,0 +1,5 @@
+console.log('Загружено characters.js')
+
+module.exports = function log() {
+  console.log('log')
+}
