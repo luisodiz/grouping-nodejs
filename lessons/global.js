f9c4150bbb4b9d1a37d6c1f314fa804f87759c57
Alexander Sosov <luisodiz@yandex.ru>
2024-03-20 22:24:20 +0300

    глобальные и модульные переменные

---
+++ lessons/global.js
@@ -0,0 +1,9 @@
+// console.log(__dirname) // укажет директорию файла
+// console.log(__filename) // укажет путь и название файла
+//
+// console.log(global)
+// console.log(performance)
+// console.log(module)
+// console.log(exports)
+
+console.log(require) // функция
